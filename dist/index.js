36f83fb8331fc294a7e61ac136c2d5657e8330f9
Danish S <danishs@devnatives.com>
2023-11-24 04:32:13 +0500

    added node mon in the code

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Decorators_1 = require("./Decorators/Decorators");
+var welcome = "Qazi Danish";
+console.log("Welcome ".concat(welcome));
+var obj = new Decorators_1.MyClass();
+obj.AddMethod(1, 11);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { MyClass } from "./Decorators/Decorators";
 const welcome: String = "Qazi Danish";
 
 console.log(`Welcome ${welcome}`);
+
+const obj: MyClass = new MyClass();
+obj.AddMethod(1, 33);
